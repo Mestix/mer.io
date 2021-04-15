ecdb0a90d4a54a03e44a17dd0d07b43f3adcbdf5
Melistick <Melistick@users.noreply.github.com>
2021-04-15 13:52:50 +0200

    delete  setup file

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-   name='Mer.io',
-   version='0.2',
-   description='<er Extraction tool>',
-   author='Melissa Landwerd',
-   install_requires=['pandas']
-)
