4411eaf92805ccdf18251ca6004ef67fc3bbb1e9
Melistick <Melistick@users.noreply.github.com>
2021-04-15 13:50:50 +0200

    add setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+   name='Mer.io',
+   version='0.2',
+   description='<er Extraction tool>',
+   author='Melissa Landwerd',
+   install_requires=['pandas']
+)
